824a4b203482c2ccf7c56db89df9707d3104e480
Matteo Veroni <matver87@gmail.com>
2018-04-28 13:54:21 +0200

    Second commit

--- src/main/java/com/matteoveroni/gwtlayout/client/GwtLayoutEntryPoint.java
+++ src/main/java/com/matteoveroni/gwtlayout/client/GwtLayoutEntryPoint.java
@@ -2,11 +2,17 @@ package com.matteoveroni.gwtlayout.client;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.RootPanel;
 
-public class GwtLayoutEntryPoint implements EntryPoint{
+public class GwtLayoutEntryPoint implements EntryPoint {
 
 	@Override
 	public void onModuleLoad() {
-		Window.alert("Hello world!");
+		FlowPanel div = new FlowPanel();
+		div.getElement().setInnerText("Hello world");
+		RootPanel.get().add(div);
+
+		Window.alert(RootPanel.getBodyElement().getInnerHTML());
 	}
 }
