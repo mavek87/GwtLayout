5d0b55d9215793d2f0fa7ccf1c73ca25f1212b2b
Matteo Veroni <matver87@gmail.com>
2018-04-28 12:48:10 +0200

    First commit

---
+++ src/main/java/com/matteoveroni/gwtlayout/client/GwtLayoutEntryPoint.java
@@ -0,0 +1,12 @@
+package com.matteoveroni.gwtlayout.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.Window;
+
+public class GwtLayoutEntryPoint implements EntryPoint{
+
+	@Override
+	public void onModuleLoad() {
+		Window.alert("Hello world!");
+	}
+}
